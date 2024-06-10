eb7164a84e41271b37ff1fe3978191424b1d3d1d
larry <larrytientcheu1@gmail.com>
2024-06-11 01:10:18 +0200

    working refqctor v3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cv2
-import download_helper, flux_loader, yolo
+from  yoloV3 import YoloModelv3
 
 
 class ObjectDetectionSystem:
@@ -7,53 +7,11 @@ class ObjectDetectionSystem:
         """
         Initialise le système de détection d'objets avec la source vidéo ou image et les paramètres de YOLO.
         """
-        self.video_loader = flux_loader.FluxLoader(source)
-        self.yolo_detector = yolo.YoloModel(yolo_config, yolo_weights, yolo_names)
-
-    def make_detection(self, frame):
-        detections = self.yolo_detector.detect(frame)
-        for box, class_id in detections:
-            x, y, w, h = box
-            label = str(self.yolo_detector.classes[class_id])
-            if label in ["mouse", "laptop", "keyboard"]:  # Les objets spécifiques
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.putText(
-                    frame,
-                    label,
-                    (x, y - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.9,
-                    (0, 255, 0),
-                    2,
-                )
-        cv2.imshow("Detection", frame)
-        return frame
-
-    def run(self):
-        """
-        Exécute le système de détection d'objets.
-        """
-        if self.video_loader.is_image:
-            frame = self.video_loader.image
-            frame = self.make_detection(frame)
-            
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
-        else:
-            while True:
-                ret, frame = self.video_loader.cap.read()
-                if not ret:
-                    break
-                frame = self.make_detection(frame)
-                
-                if cv2.waitKey(1) & 0xFF == ord("q"):
-                    break
-            self.video_loader.cap.release()
-            cv2.destroyAllWindows()
+        
 
 
 # usage scenario
-detection_system = ObjectDetectionSystem(
+detection_system = YoloModelv3(
     0, "yolo/yolov3.cfg", "yolo/yolov3.weights", "yolo/coco.names"
 )
 detection_system.run()
---
+++ yoloV3.py
@@ -0,0 +1,87 @@
+import cv2
+import numpy as np
+import flux_loader, download_helper
+
+class YoloModelv3:
+    def __init__(self, source, config_path, weights_path, names_path):
+        self.video_loader = flux_loader.FluxLoader(source)
+        download_helper.YoloDownloadsV3().download()
+        
+        self.net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+        self.layer_names = self.net.getLayerNames()
+        self.output_layers = [
+            self.layer_names[i - 1] for i in self.net.getUnconnectedOutLayers()
+        ]
+        with open(names_path, "r") as f:
+            self.classes = [line.strip() for line in f.readlines()]
+
+    def detect(self, frame):
+        height, width = frame.shape[:2]
+        blob = cv2.dnn.blobFromImage(
+            frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False
+        )
+        self.net.setInput(blob)
+        outs = self.net.forward(self.output_layers)
+
+        class_ids = []
+        confidences = []
+        boxes = []
+        for out in outs:
+            for detection in out:
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                if confidence > 0.5:
+                    center_x = int(detection[0] * width)
+                    center_y = int(detection[1] * height)
+                    w = int(detection[2] * width)
+                    h = int(detection[3] * height)
+                    x = int(center_x - w / 2)
+                    y = int(center_y - h / 2)
+                    boxes.append([x, y, w, h])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+        indices = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+        return [(boxes[i], class_ids[i]) for i in indices]
+
+
+    def make_detection(self, frame):
+        detections = self.detect(frame)
+        for box, class_id in detections:
+            x, y, w, h = box
+            label = str(self.classes[class_id])
+            if label in ["mouse", "laptop", "keyboard"]:  # Les objets spécifiques
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.putText(
+                    frame,
+                    label,
+                    (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.9,
+                    (0, 255, 0),
+                    2,
+                )
+        cv2.imshow("Detection", frame)
+        return frame
+
+    def run(self):
+        """
+        Exécute le système de détection d'objets.
+        """
+        if self.video_loader.is_image:
+            frame = self.video_loader.image
+            frame = self.make_detection(frame)
+            
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+        else:
+            while True:
+                ret, frame = self.video_loader.cap.read()
+                if not ret:
+                    break
+                frame = self.make_detection(frame)
+                
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
+            self.video_loader.cap.release()
+            cv2.destroyAllWindows()
